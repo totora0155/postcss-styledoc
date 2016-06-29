eb81776e768efdec1aaf78962b65eae7a0bb02f7
nju33 <jun.sasaki@mvrck.co.jp>
2016-06-29 19:03:09 +0900

    Remove init.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const styledoc = require('./build').default;
-styledoc.init = require('./build/init').default;
 styledoc.write = require('./build/write').default;
 
 Object.assign(styledoc, require('./build/constant').default);
